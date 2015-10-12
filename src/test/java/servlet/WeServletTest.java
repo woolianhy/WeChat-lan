6c9f0c8e28f76d0748b23b5fe76668f3ebb290d0
Randy <1029456341@qq.com>
2015-10-12 22:10:04 +0800

    添加Junit依赖以及WeServletTest

---
+++ src/test/java/servlet/WeServletTest.java
@@ -0,0 +1,14 @@
+package servlet;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WeServletTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
