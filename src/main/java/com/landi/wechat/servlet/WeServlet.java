44f1ec5b5760ddabc85ab93cf0ca3c79881dfcf2
Randy_landi <1029456341@qq.com>
2015-10-15 10:42:30 +0800

    WeServlet添加处理post请求

--- src/main/java/com/landi/wechat/servlet/WeServlet.java
+++ src/main/java/com/landi/wechat/servlet/WeServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 
+import com.landi.wechat.service.WeService;
 import com.landi.wechat.utils.SignUtil;
 
 /**
@@ -64,6 +65,13 @@ public class WeServlet extends HttpServlet {
 		// 将请求、响应的编码均设置为UTF-8（防止中文乱码）
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
+
+		// 调用核心业务类接收消息、处理消息
+		String respMessage = WeService.processRequest(request);
+		// 响应消息
+		PrintWriter out = response.getWriter();
+		out.print(respMessage);
+		out.close();
 	}
 
 }
